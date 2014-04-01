c96e9cd8baf7b066354f5e6fe3e2925f64086bed
Menglong Tan <tanmenglong@gmail.com>
2014-04-01 18:16:38 +0800

    add ex2

--- coursera-ml_201403/mlclass-ex1/gradientDescentMulti.m
+++ coursera-ml_201403/mlclass-ex1/gradientDescentMulti.m
@@ -11,7 +11,7 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
@@ -27,12 +27,12 @@ for iter = 1:num_iters
     end
 
     for fea = 1:feanum
-       theta(fea) = theta(fea) - alpha / m * theta_update(fea); 
+       theta(fea) = theta(fea) - alpha / m * theta_update(fea);
     end
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
